e54cf4b387fab58db3d13bae581b7a2a3931fafd
jade <jadehh@live.com>
2021-04-25 15:54:00 +0800

    update python 调用dll 中类方法

--- python_dll/main.py
+++ python_dll/main.py
@@ -7,6 +7,52 @@
 # @Software : Samples
 # @Desc     :
 from ctypes import *
-dll = windll.LoadLibrary("lib/test-src-shared.dll")
 
-print(dll)
+dll = windll.LoadLibrary("lib/test-src-shared.dll")
+print(dll.test_add(1, 2))
+
+import ctypes
+
+lib = ctypes.CDLL('lib/test-class-shared.dll')
+
+
+def opaque_ptr(name):
+    cls = type(name, (ctypes.Structure,), {})
+    return ctypes.POINTER(cls)
+
+
+class A(object):
+    _A = opaque_ptr('CPP_A')
+    lib.A_new.restype = _A
+    lib.A_new.argtypes = ctypes.c_char_p,
+    lib.A_destruct.argtypes = _A,
+    lib.A_someFunc.argtypes = _A,
+
+    def __init__(self, name, func=lib.A_new):
+        self._obj = func(name.encode('ascii'))
+
+    def __del__(self):
+        self.destruct()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.destruct()
+
+    def destruct(self, func=lib.A_destruct):
+        if self._obj:
+            func(self._obj)
+        self._obj = None
+
+    def some_func(self, func=lib.A_someFunc):
+        if not self._obj:
+            raise RuntimeError
+        func(self._obj)
+
+with A('test') as a:
+    a.some_func()
+
+if __name__ == '__main__':
+    with A('test') as a:
+        a.some_func()
