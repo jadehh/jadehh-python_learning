9dc6d14f21683a4bcce58d0d370c230374d5e71c
jade <jadehh@live.com>
2021-05-21 13:16:43 +0800

    update opencv 录制视频

--- python_opencv/python_cpu_capture.py
+++ python_opencv/python_cpu_capture.py
@@ -10,6 +10,7 @@ import cv2
 if __name__ == '__main__':
     #video_path = "rtsp://admin:samples123@192.168.35.211:554/h264/ch1/main/av_stream"
     video_path = "/home/jade/sda2/Data/TaiCang/Container/2020-09-04/front/2020-09-04-07-27-29.mp4"
+    video_path = "/mnt/f/视频数据集/芜湖验残视频数据集/2021-05-21-13-04-45-398.avi"
     capture = cv2.VideoCapture(video_path)
     index = 0
     cv2.namedWindow("cpu",0)
@@ -17,6 +18,7 @@ if __name__ == '__main__':
         ret,frame = capture.read()
         if ret is False:
             break
+        print(frame.shape)
         frame = cv2.resize(frame,(416,416))
         cv2.imwrite("result.jpg",frame)
 
--- python_opencv/python_opencv_record_video_camera.py
+++ python_opencv/python_opencv_record_video_camera.py
@@ -6,14 +6,17 @@
 # @Mailbox : jadehh@live.com
 # @Software: Samples
 # @Desc    :
-from multiprocessing import Process,Queue
+from threading import Thread
+from queue import Queue
+
 from jade import *
 
-class read_video(Process):
+class read_video(Thread):
     def __init__(self,video_path,framequeue):
         self.video_path = video_path
         self.framequeue = framequeue
-        Process.__init__(self)
+        self.index = 0
+        Thread.__init__(self)
     def run(self):
         capture = cv2.VideoCapture(self.video_path)
         print(self.video_path)
@@ -24,34 +27,47 @@ class read_video(Process):
         while capture.isOpened():
             ret,frame = capture.read()
             if ret:
-                self.framequeue.put([ret, frame])
+                if self.index % 10 == 0:
+                    self.framequeue.put([ret, frame])
             else:
                 self.framequeue.put([ret, frame])
                 break
-class record_video(Process):
+            self.index = self.index + 1
+class record_video(Thread):
     def __init__(self,framequeue):
         self.framequeue = framequeue
         self.fourcc =  cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
 
 
 
-        Process.__init__(self)
+        Thread.__init__(self)
     def run(self):
-        self.videoWriter = cv2.VideoWriter(os.path.join("data/"+GetTimeStamp()+ ".avi"),
-                                           self.fourcc, 30,
-                                           (1024, 1360))
+        capture = self.framequeue.get()
+        image = capture[1]
+        save_path = os.path.join("{}/data/").format(os.path.expanduser("~"))
+        CreateSavePath(save_path)
+        video_path = os.path.join(save_path,GetTime()+ ".avi")
+        self.videoWriter = cv2.VideoWriter(video_path,self.fourcc, 20, (image.shape[1],image.shape[0]))
         while True:
             capture = self.framequeue.get()
             ret,frame = capture[0],capture[1]
             if ret:
                 self.videoWriter.write(frame)
-                print("正在存储视频,还剩 {} ".format(self.framequeue.qsize()))
+                print("正在存储视频,还剩 {} 宽 = {} 高 = {}".format(self.framequeue.qsize(),frame.shape[1],frame.shape[0]))
             else:
                 break
 
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', type=str,help = 'please select day')
+    parser.add_argument('-username',type=str,help='please select starttime')
+    parser.add_argument('-passwd',type=str,help="please select duration")
+    args = parser.parse_args()
+    print(args.ip ,args.username,args.passwd)
+
     framequeue = Queue(maxsize=200)
-    read_video_process = read_video(video_path="rtsp://admin:samples123@192.168.35.212/h264/ch1/main/av_stream",
+    read_video_process = read_video(video_path="rtsp://{}:{}@{}/h264/ch1/main/av_stream".format(args.username,args.passwd,args.ip),
                                     framequeue=framequeue)
     read_video_process.start()
     record_video_process = record_video(framequeue)
