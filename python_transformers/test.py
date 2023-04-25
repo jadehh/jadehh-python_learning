da4913fd8e41cace223a08213cdb04964e624e4e
jade <jadehh@live.com>
2023-04-25 10:15:19 +0800

    update python_chart update python_ffmpeg update python_qt

---
+++ python_transformers/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File     : test.py
+# @Author   : jade
+# @Date     : 2022/5/10 9:57
+# @Email    : jadehh@1ive.com
+# @Software : Samples
+# @Desc     :
